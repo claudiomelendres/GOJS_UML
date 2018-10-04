77e23fb5f95881722b8df7fdcc9aa0e9e5d14eca
Claudio Melendres <claudiomelendres@gmail.com>
2018-10-03 23:36:59 -0400

    1 uml adding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DiagramEditorComponent } from './diagram-editor/diagram-editor.component';
+import { BasicComponent } from './basic/basic.component';
+import { MinimalComponent } from './minimal/minimal.component';
+import { UmlComponent } from './uml/uml.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiagramEditorComponent,
+    BasicComponent,
+    MinimalComponent,
+    UmlComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/minimal/minimal.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import * as go from 'gojs';
+
+@Component({
+  selector: 'app-minimal',
+  templateUrl: './minimal.component.html',
+  styleUrls: ['./minimal.component.css']
+})
+export class MinimalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+    const $ = go.GraphObject.make;  // for conciseness in defining templates
+    const myDiagram = $(go.Diagram, 'myDiagramDiv',  // create a Diagram for the DIV HTML element
+                  {
+                    initialContentAlignment: go.Spot.Center,  // center the content
+                    'undoManager.isEnabled': true  // enable undo & redo
+                  });
+    // define a simple Node template
+    myDiagram.nodeTemplate =
+      $(go.Node, 'Auto',  // the Shape will go around the TextBlock
+        $(go.Shape, 'RoundedRectangle', { strokeWidth: 0, fill: 'white' },
+          // Shape.fill is bound to Node.data.color
+          new go.Binding('fill', 'color')),
+        $(go.TextBlock,
+          { margin: 8 },  // some room around the text
+          // TextBlock.text is bound to Node.data.key
+          new go.Binding('text', 'key'))
+      );
+    // but use the default Link template, by not setting Diagram.linkTemplate
+    // create the model data that will be represented by Nodes and Links
+    myDiagram.model = new go.GraphLinksModel(
+    [
+      { key: 'Alpha', color: 'lightblue' },
+      { key: 'Beta', color: 'orange' },
+      { key: 'Gamma', color: 'lightgreen' },
+      { key: 'Delta', color: 'pink' }
+    ],
+    [
+      { from: 'Alpha', to: 'Beta' },
+      { from: 'Alpha', to: 'Gamma' },
+      { from: 'Beta', to: 'Beta' },
+      { from: 'Gamma', to: 'Delta' },
+      { from: 'Delta', to: 'Alpha' }
+    ]);
+  }
+
+
+}
